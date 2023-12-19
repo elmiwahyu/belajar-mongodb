ea391a4a8bf91ef386f2192a23020f9b06125fa8
sadaf <137382628+elmiwahyu@users.noreply.github.com>
2023-12-19 09:45:09 +0700

    Delete belajar-mongodb-master directory

--- belajar-mongodb-master/scripts/collection.js
+++
@@ -1,8 +0,0 @@
-// Membuat Collection customers
-db.createCollection('customers');
-
-// Membuat Collection products
-db.createCollection('products');
-
-// Membuat Collection orders
-db.createCollection('orders');
